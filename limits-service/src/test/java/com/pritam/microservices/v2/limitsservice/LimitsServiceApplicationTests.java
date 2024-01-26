cbaa43db46f358bebd3520e5e1b8531d0d57e968
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-26 11:09:45 +0530

    Added limits-service project

---
+++ limits-service/src/test/java/com/pritam/microservices/v2/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
