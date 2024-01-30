8bd7a55114ee01f398b672107259f6639011683a
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-30 19:32:15 +0530

    Topic-12

--- currency-calculation-service/src/main/java/com/pritam/microservices/v2/currencycalculationservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-calculation-service/src/main/java/com/pritam/microservices/v2/currencycalculationservice/proxy/CurrencyExchangeServiceProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.pritam.microservices.v2.currencycalculationservice.model.CurrencyConversionBean;
 
-@FeignClient(name="currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retriveExchangeValue(@PathVariable String from, @PathVariable String to);
---
+++ naming-server/src/test/java/com/pritam/microservices/v2/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
