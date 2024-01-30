06b115751e6236bf6f1463d88db259b8ea2a6c41
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-30 16:10:52 +0530

    Topic-3

---
+++ config-server/src/main/java/com/pritam/microservices/v2/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
