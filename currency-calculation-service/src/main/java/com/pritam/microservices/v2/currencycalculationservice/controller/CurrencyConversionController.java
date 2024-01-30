2cf363e26bbaf158e8763978eb5c4482cd1290fa
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-30 18:51:27 +0530

    Topic-10

--- currency-calculation-service/src/main/java/com/pritam/microservices/v2/currencycalculationservice/controller/CurrencyConversionController.java
+++ currency-calculation-service/src/main/java/com/pritam/microservices/v2/currencycalculationservice/controller/CurrencyConversionController.java
@@ -1,8 +1,13 @@
 package com.pritam.microservices.v2.currencycalculationservice.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.pritam.microservices.v2.currencycalculationservice.model.CurrencyConversionBean;
 
@@ -11,6 +16,15 @@ public class CurrencyConversionController {
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean retriveCurrencyConversionBean(@PathVariable String from, @PathVariable String to, @PathVariable double quantity) {
-		return new CurrencyConversionBean(1,from,to,1,quantity,8100);
+		Map<String,String> uriVariables = new HashMap<String,String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,uriVariables);
+		CurrencyConversionBean currencyConversionBean = responseEntity.getBody();
+		if(currencyConversionBean!=null) {
+			currencyConversionBean.setQuantity(quantity);
+			currencyConversionBean.setTotalCalculatedAmount();
+		}
+		return currencyConversionBean;
 	}
 }
