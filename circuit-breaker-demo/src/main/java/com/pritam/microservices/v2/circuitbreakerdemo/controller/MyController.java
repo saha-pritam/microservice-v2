5f1b432464cb14569476790f586a1b8f7cbb6575
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-31 17:41:24 +0530

    Topic-22-f

--- circuit-breaker-demo/src/main/java/com/pritam/microservices/v2/circuitbreakerdemo/controller/MyController.java
+++ circuit-breaker-demo/src/main/java/com/pritam/microservices/v2/circuitbreakerdemo/controller/MyController.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -13,7 +15,8 @@ public class MyController {
 	private Logger logger = LoggerFactory.getLogger(MyController.class);
 	
 	@GetMapping("/myapi-1")
-	@Retry(name="myapi1",fallbackMethod = "onFailure")
+	//@Retry(name="myapi1",fallbackMethod = "onFailure")
+	@CircuitBreaker(name="myapi1",fallbackMethod = "onFailure")
 	public String myApi1() {
 		logger.info("====INSIDE MY API 1====");
 		throw new ArithmeticException("Exception at my api 1");
@@ -21,8 +24,11 @@ public class MyController {
 	
 	@GetMapping("/myapi-2")
 	public String myApi2() {
-		logger.info("====INSIDE MY API 2====");
-		throw new RuntimeException("Exception at my api 2");
+		RestTemplate restTemplate = new RestTemplate();
+		for(;;) {
+			restTemplate.getForEntity("http://localhost:8080/myapi-1", String.class);
+		}
+			
 	}
 	
 	private String onFailure(RuntimeException re) {
