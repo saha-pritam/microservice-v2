6747137f491229594c66a8001abacd40823016f8
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-31 16:56:50 +0530

    Topic-21

---
+++ circuit-breaker-demo/src/main/java/com/pritam/microservices/v2/circuitbreakerdemo/CircuitBreakerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.circuitbreakerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerDemoApplication.class, args);
+	}
+
+}
---
+++ circuit-breaker-demo/src/test/java/com/pritam/microservices/v2/circuitbreakerdemo/CircuitBreakerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.circuitbreakerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
