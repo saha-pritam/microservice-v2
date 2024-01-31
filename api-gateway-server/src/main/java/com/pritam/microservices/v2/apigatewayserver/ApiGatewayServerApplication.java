beee1385513ba68bfcee3a8703892c104b18127b
Pritam Saha <saha.pritam.work@gmail.com>
2024-01-31 15:06:44 +0530

    Topic-14

---
+++ api-gateway-server/src/main/java/com/pritam/microservices/v2/apigatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+}
---
+++ api-gateway-server/src/test/java/com/pritam/microservices/v2/apigatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pritam.microservices.v2.apigatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
